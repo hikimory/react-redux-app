3cc2970f09324dc32949d3da58f664ac66a52503
Nick Bykon <nikita.bikon@gmail.com>
2020-08-15 20:00:28 +0300

    add custom middleware

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import {render} from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { rootReducer } from './redux/rootReducer';
 import thunk from 'redux-thunk'
 import { createStore, compose , applyMiddleware} from 'redux';
 import { Provider } from 'react-redux';
+import { forbiddenWordsMiddleware } from './redux/middleware';
 
 const store = createStore(rootReducer, compose(
   applyMiddleware(
-    thunk
+    thunk,
+    forbiddenWordsMiddleware
   ),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
